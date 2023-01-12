d26dd4c00eec09833f8112bf7469590ef5c07b70
GetPsyched <priyanshutr@gmail.com>
2023-01-12 18:45:49 +0530

    Connect MongoDB database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => app.listen(PORT, () => console.log("Database connected!")))
+  .catch((err) => console.log(err.message));
