11d9a4493db933d37474e4a4fed8c554c97adb7c
GetPsyched <priyanshutr@gmail.com>
2023-01-17 16:32:54 +0530

    Update App.js with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import RequireAuth from "./components/RequireAuth.js";
+import Home from "./components/Home.js";
+import ListStudent from "./components/ListStudent";
+import CreateStudent from "./components/CreateStudent";
+import Missing from "./components/Missing.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MERN Assignment</h1>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+
+        {/* private routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route element={<RequireAuth />}>
+          <Route path="/students" element={<ListStudent />} />
+        </Route>
+        <Route element={<RequireAuth />}>
+          <Route path="/students/new" element={<CreateStudent />} />
+        </Route>
+
+        {/* default route */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
