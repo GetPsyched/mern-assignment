b1bf13a6196a6851d2742aecdd2f5f24579aaed7
GetPsyched <priyanshutr@gmail.com>
2023-01-21 17:07:43 +0530

    Add instructions on Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,25 @@
+import { Typography } from '@mui/material';
 import NavBar from './NavBar.js';
 
 const Home = () => {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{
+          flexGrow: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        You're on the home page. Click the hamburger icon on the top left to
+        navigate to the other pages
+      </Typography>
+    </>
+  );
 };
 
 export default Home;
