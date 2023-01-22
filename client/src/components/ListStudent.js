58f0dd99459da0c913567ad115f0c30a3606d7db
GetPsyched <priyanshutr@gmail.com>
2023-01-22 19:10:45 +0530

    Update table library

--- client/src/components/ListStudent.js
+++ client/src/components/ListStudent.js
@@ -1,230 +1,74 @@
 import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
-import { createSearchParams, useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
-import {
-  Delete as DeleteIcon,
-  Edit as EditIcon,
-  FirstPage as FirstPageIcon,
-  KeyboardArrowLeft,
-  KeyboardArrowRight,
-  LastPage as LastPageIcon,
-} from '@mui/icons-material';
 import { Container } from '@material-ui/core';
-import {
-  Box,
-  IconButton,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableFooter,
-  TableHead,
-  TablePagination,
-  TableRow,
-} from '@mui/material';
+import MaterialTable from 'material-table';
 import NavBar from './NavBar';
-
-function TablePaginationActions(props) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === 'rtl' ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </Box>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
+import { ThemeProvider } from '@mui/material/styles';
 
 export default function ListStudent() {
   const [rows, setRows] = useState([]);
-  const [update, setUpdate] = useState(false);
   const axiosPrivate = useAxiosPrivate();
-  const navigate = useNavigate();
+  const theme = useTheme();
 
-  const updateStudent = (student) => {
-    navigate({
-      pathname: '/students/update',
-      search: createSearchParams({
-        id: student._id,
-        gr: student.generalRegistrationNumber,
-        name: student.name,
-        email: student.email,
-        address: student.address,
-        city: student.city,
-        grade: student.grade,
-        section: student.section,
-      }).toString(),
-    });
-  };
-  const deleteStudent = (id) => {
-    axiosPrivate.delete(`/students/${id}`).then(() => setUpdate(!update));
-  };
   useEffect(() => {
     axiosPrivate.get('/students').then((students) => {
       setRows(students.data);
     });
-  }, [update]);
-
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+  }, []);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <NavBar />
       <Container>
         <h2>Student List</h2>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">GR No.</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Address Line</TableCell>
-                <TableCell>City</TableCell>
-                <TableCell align="right">Grade</TableCell>
-                <TableCell>Section</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {(rowsPerPage > 0
-                ? rows.slice(
-                    page * rowsPerPage,
-                    page * rowsPerPage + rowsPerPage
-                  )
-                : rows
-              ).map((student) => (
-                <TableRow key={student.generalRegistrationNumber}>
-                  <TableCell align="right" component="th" scope="row">
-                    {student.generalRegistrationNumber}
-                  </TableCell>
-                  <TableCell>{student.name}</TableCell>
-                  <TableCell>{student.email}</TableCell>
-                  <TableCell>{student.address}</TableCell>
-                  <TableCell>{student.city}</TableCell>
-                  <TableCell align="right">{student.grade}</TableCell>
-                  <TableCell>{student.section}</TableCell>
-                  <TableCell>
-                    <IconButton
-                      aria-label="update"
-                      onClick={() => updateStudent(student)}
-                    >
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton
-                      aria-label="delete"
-                      onClick={() => deleteStudent(student._id)}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 53 * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                  colSpan={3}
-                  count={rows.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  SelectProps={{
-                    inputProps: {
-                      'aria-label': 'rows per page',
-                    },
-                    native: true,
-                  }}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                  ActionsComponent={TablePaginationActions}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
+        <MaterialTable
+          title="Student List"
+          columns={[
+            {
+              title: 'GR No.',
+              field: 'generalRegistrationNumber',
+              type: 'numeric',
+            },
+            { title: 'Name', field: 'name' },
+            { title: 'Email', field: 'email' },
+            { title: 'Address Line', field: 'address' },
+            { title: 'City', field: 'city' },
+            { title: 'Grade', field: 'grade', type: 'numeric' },
+            { title: 'Section', field: 'section' },
+          ]}
+          data={rows}
+          options={{
+            filtering: true,
+            actionsColumnIndex: -1,
+          }}
+          theme={theme}
+          editable={{
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, _) => {
+                axiosPrivate.patch(`/students`, newData).then(() => {
+                  const dataUpdate = [...rows];
+                  const index = oldData.tableData.id;
+                  dataUpdate[index] = newData;
+                  setRows([...dataUpdate]);
+                });
+                resolve();
+              }),
+            onRowDelete: (oldData) =>
+              new Promise((resolve, _) => {
+                axiosPrivate
+                  .delete(`/students/${oldData._id}`)
+                  .then((response) => {
+                    const dataDelete = [...rows];
+                    const index = oldData.tableData.id;
+                    dataDelete.splice(index, 1);
+                    setRows([...dataDelete]);
+                  });
+                resolve();
+              }),
+          }}
+        />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
